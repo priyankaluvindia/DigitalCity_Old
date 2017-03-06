bf12ec02e25fc6ce979bb2b661c7d6f4605804e4
Priyanka Pandey <priyankaluvindia@gmail.com>
2017-03-06 12:59:25 +0530

    workmodels commit

---
+++ WorkModels/SubCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkModels
+{
+    public class SubCategory
+    {
+        public int SUBCATEGORYID { get; set; }
+        public string NAME { get; set; }
+        public Nullable<System.DateTime> CREATEDON { get; set; }
+        public string STATUS { get; set; }
+        public string IMAGEURL { get; set; }
+        public Nullable<int> PRODUCTS { get; set; }
+        public string SYNONYMS { get; set; }
+        public Nullable<int> CATEGORYID { get; set; }
+    }
+}
