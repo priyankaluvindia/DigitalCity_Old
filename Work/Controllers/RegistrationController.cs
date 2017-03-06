0dc8b37a291a4e7d4884e53d92f19e3d4ab56ecd
Priyanka Pandey <priyankaluvindia@gmail.com>
2017-03-06 12:48:21 +0530

    work h

--- Work/Controllers/RegistrationController.cs
+++ Work/Controllers/RegistrationController.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using WorkModels;
-using WorkRepo;
-namespace Work.Controllers
-{
-    public class RegistrationController : ApiController
-    {
-        RegistrationRepo rep = new RegistrationRepo();
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
+//using WorkModels;
+//using WorkRepo;
+//namespace Work.Controllers
+//{
+//    public class RegistrationController : ApiController
+//    {
+//        RegistrationRepo rep = new RegistrationRepo();
 
-        // GET: api/Registration/5
-        public string Get(String id)
-        {
-           if( rep.VerifyUser(id).Result)
-            {
-                return "Already Taken";
-            }
-           else
-            {
-                return "Available";
-            }
-        }
+//        // GET: api/Registration/5
+//        public string Get(String id)
+//        {
+//           if( rep.VerifyUser(id).Result)
+//            {
+//                return "Already Taken";
+//            }
+//           else
+//            {
+//                return "Available";
+//            }
+//        }
 
-        // POST: api/Registration
-        public void Post([FromBody]string value)
-        {
-        }
+//        // POST: api/Registration
+//        public void Post([FromBody]string value)
+//        {
+//        }
 
-        // PUT: api/Registration/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+//        // PUT: api/Registration/5
+//        public void Put(int id, [FromBody]string value)
+//        {
+//        }
 
-        // DELETE: api/Registration/5
-        public void Delete(int id)
-        {
-        }
-    }
-}
+//        // DELETE: api/Registration/5
+//        public void Delete(int id)
+//        {
+//        }
+//    }
+//}
---
+++ Work/Controllers/SubCategoryController.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WorkModels;
+using WorkRepo;
+
+namespace Work.Controllers
+{
+    public class SubCategoryController : ApiController
+    {
+        private SubCategoryRepo subcategoryRepo = new SubCategoryRepo();
+
+        public List<SubCategory> Get()
+        {
+            return subcategoryRepo.GetSubCategories().Result;
+        }
+
+        public async Task<IHttpActionResult> Post()
+        {
+            HttpPostedFile file = null;
+            String location = "";
+            if (HttpContext.Current.Request.Files.Count > 0)
+            {
+                file = HttpContext.Current.Request.Files[0];
+                location = "/images/subcategories/" + Guid.NewGuid() + DateTime.Now.ToString("ddMMyyyyHHmmss") + Path.GetExtension(file.FileName);
+            }
+            SubCategory subcategory = (SubCategory)JsonConvert.DeserializeObject(HttpContext.Current.Request.Form[0], typeof(SubCategory));
+            if (file != null)
+            {
+                subcategory.IMAGEURL = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority + "" + location;
+            }
+            if (await subcategoryRepo.SaveSubCategory(subcategory))
+            {
+                if (file != null)
+                {
+                    try
+                    {
+                        String path = HttpContext.Current.Server.MapPath("~/") + location;
+                        file.SaveAs(path);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+                return Ok(subcategory);
+            }
+            else
+            {
+                return BadRequest("Record Cannot Saved");
+            }
+        }
+
+    }
+}
