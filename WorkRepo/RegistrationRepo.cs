3d45909f3ae92c62fcbaeeb81b8acfb5779d8ca5
Priyanka Pandey <priyankaluvindia@gmail.com>
2017-03-06 13:00:25 +0530

    work repo commit

--- WorkRepo/RegistrationRepo.cs
+++ WorkRepo/RegistrationRepo.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WorkData;
-namespace WorkRepo
-{
-    public class RegistrationRepo
-    {
-        public async Task<bool> VerifyUser(String username)
-        {
-            WorkEntities db = new WorkEntities();        
-            return await Task.FromResult<bool>( db.USERS.Any(x => x.USERNAME == username));
-        }
-    }
-}
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using WorkData;
+//namespace WorkRepo
+//{
+//    public class RegistrationRepo
+//    {
+//        public async Task<bool> VerifyUser(String username)
+//        {
+//            WorkEntities db = new WorkEntities();        
+//            return await Task.FromResult<bool>( db.USERS.Any(x => x.USERNAME == username));
+//        }
+//    }
+//}
---
+++ WorkRepo/SubCategoryRepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkData;
+using WorkModels;
+
+namespace WorkRepo
+{
+    public class SubCategoryRepo
+    {
+        private WorkEntities db = new WorkEntities();
+
+        public async Task<List<SubCategory>> GetSubCategories()
+        {
+            return await Task.FromResult(db.SUBCATEGORies.Select(x => new SubCategory { SUBCATEGORYID = x.SUBCATEGORYID, NAME = x.NAME, CREATEDON = x.CREATEDON, STATUS = x.STATUS, IMAGEURL = x.IMAGEURL, PRODUCTS = x.PRODUCTS, SYNONYMS = x.SYNONYMS, CATEGORYID = x.CATEGORYID }).ToList());
+        }
+
+        public async Task<bool> SaveSubCategory(SubCategory modelSubCategory)
+        {
+            var subcategory = db.SUBCATEGORies.FirstOrDefault(x => x.SUBCATEGORYID == modelSubCategory.SUBCATEGORYID);
+            if (subcategory == null)
+            {
+                subcategory = new SUBCATEGORY
+                {
+                    NAME=modelSubCategory.NAME,
+                    CREATEDON=DateTime.Now,
+                    STATUS="Active",
+                    IMAGEURL=modelSubCategory.IMAGEURL,
+                    PRODUCTS=0,
+                    SYNONYMS=modelSubCategory.SYNONYMS,
+                    CATEGORYID=modelSubCategory.CATEGORYID
+                };
+                db.SUBCATEGORies.Add(subcategory);
+            }
+            try
+            {
+                await db.SaveChangesAsync();
+                modelSubCategory.SUBCATEGORYID = subcategory.SUBCATEGORYID;
+                modelSubCategory.CREATEDON = subcategory.CREATEDON;
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
