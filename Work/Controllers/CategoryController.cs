0be121007dd7bb74bd750b095ea66d1925ca8d4c
priyankaluvindia <priyankaluvindia@gmail.com>
2017-03-15 23:42:34 +0530

    h

--- Work/Controllers/CategoryController.cs
+++ Work/Controllers/CategoryController.cs
@@ -16,23 +16,17 @@ namespace Work.Controllers
     public class CategoryController : ApiController
     {
         private CategoryRepo repo = new CategoryRepo();
+
         // GET: api/Category
-        public  List<Category> Get()
+        public List<Category> Get()
         {
             return repo.GetCategories().Result;
         }
 
-        public String Index(string title,string id) {
-            return null;
-        }
-
-        // GET: api/Category/5
-        public string Get(int id)
+        public Category Get(int id)
         {
-            return "value";
+            return repo.GetCategoryById(id).Result;
         }
-
-
 
         // POST: api/Category
         public async Task<IHttpActionResult> Post()
@@ -42,16 +36,16 @@ namespace Work.Controllers
             if (HttpContext.Current.Request.Files.Count > 0)
             {
                 file = HttpContext.Current.Request.Files[0];
-                location="/images/categories/"+ Guid.NewGuid() + DateTime.Now.ToString("ddMMyyyyHHmmss") + Path.GetExtension(file.FileName);
+                location = "/images/categories/" + Guid.NewGuid() + DateTime.Now.ToString("ddMMyyyyHHmmss") + Path.GetExtension(file.FileName);
             }
-       
+
             Category category = (Category)JsonConvert.DeserializeObject(HttpContext.Current.Request.Form[0], typeof(Category));
-       
-            if(file!=null)
+
+            if (file != null)
             {
-                category.IMAGEURL = HttpContext.Current.Request.Url.Scheme+"://"+ HttpContext.Current.Request.Url.Authority + "" + location;
+                category.IMAGEURL = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority + "" + location;
             }
-           
+
             if (await repo.SaveCategory(category))
             {
                 if (file != null)
@@ -61,7 +55,7 @@ namespace Work.Controllers
                         String path = HttpContext.Current.Server.MapPath("~/") + location;
                         file.SaveAs(path);
                     }
-                    catch(Exception ex)
+                    catch (Exception ex)
                     {
 
                     }
@@ -74,5 +68,60 @@ namespace Work.Controllers
                 return BadRequest("Record can't be saved !!");
             }
         }
+
+        //put
+
+        public async Task<IHttpActionResult> Put(int id)
+        {
+            HttpPostedFile file = null;
+            String location = "";
+            if (HttpContext.Current.Request.Files.Count > 0)
+            {
+                file = HttpContext.Current.Request.Files[0];
+                location = "/images/categories/" + Guid.NewGuid() + DateTime.Now.ToString("ddMMyyyyHHmmss") + Path.GetExtension(file.FileName);
+            }
+
+            Category category = (Category)JsonConvert.DeserializeObject(HttpContext.Current.Request.Form[0], typeof(Category));
+
+            if (file != null)
+            {
+                category.IMAGEURL = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority + "" + location;
+            }
+            if (await repo.UpdateCategory(id, category))
+            {
+                if (file != null)
+                {
+                    try
+                    {
+                        String path = HttpContext.Current.Server.MapPath("~/") + location;
+                        file.SaveAs(path);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+                return Ok(category);
+            }
+
+            else
+            {
+                return BadRequest("Record can't be Updated !!");
+            }
+        }
+
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            if (await repo.DeleteCategory(id))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Record can't be Deleted !!");
+            }
+        }
+
+
     }
 }
--- WorkRepo/CategoryRepo.cs
+++ WorkRepo/CategoryRepo.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using WorkData;
 using WorkModels;
 namespace WorkRepo
@@ -15,9 +18,10 @@ namespace WorkRepo
             var category = context.CATEGORIES.FirstOrDefault(x => x.ID == model.ID);
             if (category == null)
             {
-                category = new CATEGORy { SYNONYMS=model.SYNONYMS, NAME = model.NAME, CREATEDON = DateTime.Now, STATUS = "Active", IMAGEURL = model.IMAGEURL, PRODUCTS = 0, SUBCATEGORIES = 0 };
+                category = new CATEGORy { SYNONYMS = model.SYNONYMS, NAME = model.NAME, CREATEDON = DateTime.Now, STATUS = "Active", IMAGEURL = model.IMAGEURL, PRODUCTS = 0, SUBCATEGORIES = 0 };
                 context.CATEGORIES.Add(category);
             }
+
             try
             {
                 await context.SaveChangesAsync();
@@ -25,7 +29,7 @@ namespace WorkRepo
                 model.CREATEDON = category.CREATEDON;
                 return true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return false;
             }
@@ -33,7 +37,81 @@ namespace WorkRepo
 
         public async Task<List<Category>> GetCategories()
         {
-            return await Task.FromResult(context.CATEGORIES.Select(x => new Category { ID = x.ID, CREATEDON = x.CREATEDON, IMAGEURL = x.IMAGEURL, NAME = x.NAME, PRODUCTS = x.PRODUCTS, STATUS = x.STATUS, SUBCATEGORIES = x.SUBCATEGORIES, SYNONYMS=x.SYNONYMS }).ToList());
+            return await Task.FromResult(context.CATEGORIES.Select(x => new Category { ID = x.ID, CREATEDON = x.CREATEDON, IMAGEURL = x.IMAGEURL, NAME = x.NAME, PRODUCTS = x.PRODUCTS, STATUS = x.STATUS, SUBCATEGORIES = x.SUBCATEGORIES, SYNONYMS = x.SYNONYMS }).ToList());
         }
+
+        public async Task<Category> GetCategoryById(int id)
+        {
+            return await Task.FromResult(context.CATEGORIES.Select(x => new Category { ID = x.ID, CREATEDON = x.CREATEDON, IMAGEURL = x.IMAGEURL, NAME = x.NAME, PRODUCTS = x.PRODUCTS, STATUS = x.STATUS, SUBCATEGORIES = x.SUBCATEGORIES, SYNONYMS = x.SYNONYMS }).FirstOrDefault(e => e.ID == id));
+        }
+
+        public async Task<bool> DeleteCategory(int id)
+        {
+            var category = context.CATEGORIES.FirstOrDefault(x => x.ID == id);
+            if (category != null)
+            {
+                if ((!String.IsNullOrEmpty(category.IMAGEURL)))
+                {
+                    String path = HttpContext.Current.Server.MapPath("~/") + category.IMAGEURL.Substring(category.IMAGEURL.IndexOf("images"));
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+
+                }
+                List<SUBCATEGORY > subcat=context.SUBCATEGORies.Where(x=>x.CATEGORYID==category.ID).ToList();
+                context.SUBCATEGORies.RemoveRange(subcat);
+                context.CATEGORIES.Remove(category);
+            }
+
+            try
+            {
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateCategory(int id, Category model)
+        {
+            var category = context.CATEGORIES.FirstOrDefault(x => x.ID == id);
+            if (category != null)
+            {
+                category.ID = model.ID;
+                category.NAME = model.NAME;
+                if ((!String.IsNullOrEmpty(model.IMAGEURL)) && (!String.IsNullOrEmpty(category.IMAGEURL)) && model.IMAGEURL != category.IMAGEURL)
+                {
+                    String path = HttpContext.Current.Server.MapPath("~/") + category.IMAGEURL.Substring(category.IMAGEURL.IndexOf("images"));
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                   
+                }
+                if (!String.IsNullOrEmpty(model.IMAGEURL))
+                {
+                    category.IMAGEURL = model.IMAGEURL;
+                }
+                category.SYNONYMS = model.SYNONYMS;
+                category.STATUS = model.STATUS;
+            }
+            try
+            {
+                await context.SaveChangesAsync();              
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+
     }
 }
